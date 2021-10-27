3e47100a9dcc01607e0be46e7b3ca998c249590a
Louis Burda <quent.burda@gmail.com>
2021-10-27 03:03:26 +0200

    increase workers, bump gmpy2 version for which wheel is available
    ..so that we can remove c lib dependency for building one in docker

--- checker/src/gunicorn.conf.py
+++ checker/src/gunicorn.conf.py
@@ -1,8 +1,10 @@
 import multiprocessing
 
 worker_class = "uvicorn.workers.UvicornWorker"
-workers = min(4, multiprocessing.cpu_count())
+workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:3031"
 timeout = 90
 keepalive = 3600
 preload_app = True
+# max_requests = 200
+# max_requests_jitter = 30
