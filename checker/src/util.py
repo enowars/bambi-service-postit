146e0fbe562abbb19a44e3826444431f9bc5fcd0
Louis Burda <quent.burda@gmail.com>
2021-10-29 13:41:45 +0200

    add fluff to username and noise generation

--- checker/src/util.py
+++ checker/src/util.py
@@ -1,11 +1,51 @@
 #!/usr/bin/env python3
 
 import math
-import random
+import secrets
 import string
 import time
+from base64 import b64encode
 from logging import LoggerAdapter
 from typing import Any
+
+leetconv = {
+    "O": "0",
+    "l": "1",
+    "I": "1",
+    "Z": "2",
+    "E": "3",
+    "A": "4",
+    "S": "5",
+    "G": "6",
+    "T": "7",
+}
+
+wordlist = open("media/wordlist").read().split("\n")
+names = [line for line in wordlist if line != ""]
+rickroll = open("media/rickroll.b64").read()
+messages = [
+    "Remember: invite Paul to lan party",
+    "Shopping list: tomatoes and potatoes",
+    "This is not the flag hehe",
+    "🤓🤓🤓 Try Harder 🤓🤓🤓",
+    "Crypto makes me go 🥴🤢🤮",
+    "RSA, more like (R)eal (S)Bad (A)Crypto",
+    "🤡 The flag is in another castle! 🤡",
+    "🧠 solving crypto challenges calculator 🧠",
+]
+
+
+def randint(low: int, high: int) -> int:
+    return low + secrets.randbelow(high - low + 1)
+
+
+def randbool() -> bool:
+    return randint(0, 1) == 1
+
+
+def randstr(n: int) -> str:
+    chars = [secrets.choice(string.ascii_letters + string.digits) for i in range(n)]
+    return "".join(chars)
 
 
 def bytes2int(s: bytes) -> int:
@@ -13,20 +53,43 @@ def bytes2int(s: bytes) -> int:
 
 
 def int2bytes(i: int) -> bytes:
-    blen = math.ceil(math.log2(i) / 8)
+    assert i >= 0
+    blen = 1 if i == 0 else math.ceil(math.log2(i) / 8)
     return int.to_bytes(i, byteorder="big", length=blen)
 
 
+def spongebobcase(name: str) -> str:
+    return "".join([(c.upper() if randbool() else c) for c in name])
+
+
+def leetify(name: str) -> str:
+    if randbool():
+        name = spongebobcase(name)
+    return "".join([(leetconv[c] if c in leetconv else c) for c in name])
+
+
 def gen_username() -> bytes:
-    # TODO: fluff
-    chars = [random.choice(string.ascii_letters) for i in range(32)]
-    return "".join(chars).encode()
+    msg = ""
+    while len(msg) < randint(10, 30):
+        part = secrets.choice(names)
+        if randbool():
+            part = leetify(part)
+        msg += part
+    username = msg + randstr(30)
+    return username.encode()
 
 
 def gen_noise() -> bytes:
-    # TODO: fluff, rick-roll b64 image, ...
-    chars = [random.choice(string.ascii_letters) for i in range(32)]
-    return "".join(chars).encode()
+    selection = randint(0, len(messages) + 1)
+    if selection == 0:
+        msg = randstr(randint(30, 60))
+    elif selection == 1:
+        msg = rickroll
+    else:
+        msg = secrets.choice(messages)
+        if randbool():
+            msg = b64encode(msg.encode()).decode()
+    return msg.encode()
 
 
 async def timed(promise: Any, logger: LoggerAdapter, ctx: str) -> Any:
