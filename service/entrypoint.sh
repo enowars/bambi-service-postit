44605763438b0f54949679bace34b01d8c1ef7f9
Louis Burda <quent.burda@gmail.com>
2021-10-29 15:04:06 +0200

    move data directory into /service for correct perms

--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ ! -e "/data/db.sqlite3" ]; then
-    /service/gendb /data/db.sqlite3
+if [ ! -e "/service/data/db.sqlite3" ]; then
+    /service/gendb /service/data/db.sqlite3
 fi
 
 while [ 1 ]; do
 	echo "[DB CLEANUP] @ $(date +%T)"
-	/service/cleandb /data/db.sqlite3
+	/service/cleandb /service/data/db.sqlite3
 	sleep 60
 done &
 
 ncat --keep-open --listen -p 9000 --no-shutdown \
-    --wait 10s --sh-exec '/service/postit /data/db.sqlite3'
+    --wait 10s --sh-exec '/service/postit /service/data/db.sqlite3'
